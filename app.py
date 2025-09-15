ee5463f25407fa17458fd48f43c9480bb12d3a2d
Eduarda Azeredo Magnago <157523329+eduarda-magnago@users.noreply.github.com>
2025-09-15 14:56:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,6 @@ def deletar_mensagem(index):
 
 if __name__ == "__main__":
     print("Device set to use cpu")
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080)) 
     app.run(host="0.0.0.0", port=port)
 
